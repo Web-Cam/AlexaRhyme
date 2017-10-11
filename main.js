decc29782655d501519081c5882ba43aa590afd3
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2017-10-11 19:20:44 -0400

    Update main.js
    Comment out code

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var start = 'https://api.datamuse.com/words?rel_rhy=';
-var rhyme = prompt("Enter the word to rhyme with");
-var url = start + rhyme;
+var rhyme = prompt("Enter the word to rhyme with");// FUTURE ALEXA UTTERANCE
+var url = start + rhyme; // Set url to input will change later to set to an alexa utterance.
 
-
-var ourRequest = new XMLHttpRequest({mozSystem: true});
+// Code to check api against server
+var ourRequest = new XMLHttpRequest({mozSystem: true});//had to set Mozsystem to true in order not get error.
 ourRequest.open('GET', url)
 ourRequest.onload = function() {
-	var rhymes = JSON.parse(ourRequest.responseText);
-	console.log(rhymes[2].word)
-	rhymesreturn = rhymes[2].word
-	alert(rhymesreturn)
+	var rhymes = JSON.parse(ourRequest.responseText); //Recieve the JSON data
+	console.log(rhymes[2].word) //Select second rhyme from set. Need to fix to get a random rhyme.
+	rhymesreturn = rhymes[2].word //Change variable to return to make future proram easier to understand
+	alert(rhymesreturn) //Popup will change to an alexa return statement.
 };
 ourRequest.send();
