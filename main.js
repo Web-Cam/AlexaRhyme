c629616b3d0ee38f649625a6c5278084b0085d41
Ilan Biala <ilan.biala@gmail.com>
2017-10-11 19:40:31 -0400

    Update main.js
    A random rhyming word is now picked from the returned JSON

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 var start = 'https://api.datamuse.com/words?rel_rhy=';
-var rhyme = prompt("Enter the word to rhyme with");// FUTURE ALEXA UTTERANCE
+var rhyme = prompt('Enter the word to rhyme with'); // FUTURE ALEXA UTTERANCE
 var url = start + rhyme; // Set url to input will change later to set to an alexa utterance.
 
 // Code to check api against server
-var ourRequest = new XMLHttpRequest({mozSystem: true});//had to set Mozsystem to true in order not get error.
-ourRequest.open('GET', url)
+var ourRequest = new XMLHttpRequest({ mozSystem: true }); // Set Mozsystem to true in order not get error.
+ourRequest.open('GET', url);
 ourRequest.onload = function() {
-	var rhymes = JSON.parse(ourRequest.responseText); //Recieve the JSON data
-	console.log(rhymes[2].word) //Select second rhyme from set. Need to fix to get a random rhyme.
-	rhymesreturn = rhymes[2].word //Change variable to return to make future proram easier to understand
-	alert(rhymesreturn) //Popup will change to an alexa return statement.
+	var rhymes = JSON.parse(ourRequest.responseText); // Receive JSON data
+	var randomIndex = Math.floor(Math.random() * rhymes.length);
+	console.log(rhymes[randomIndex].word); // Pick random rhyme from JSON
+	result = rhymes[randomIndex].word;
+	alert(result); // Popup will change to an alexa return statement.
 };
 ourRequest.send();
