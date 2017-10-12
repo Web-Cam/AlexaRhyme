355b11b5a89033dfe71fcad014cfe05adf91f9e7
Ilan Biala <ilan.biala@gmail.com>
2017-10-11 22:14:20 -0400

    Update main.js
    Wrap functionality in function that returns Array of n random rhyming words.

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-var start = 'https://api.datamuse.com/words?rel_rhy=';
-var rhyme = prompt('Enter the word to rhyme with'); // FUTURE ALEXA UTTERANCE
-var url = start + rhyme; // Set url to input will change later to set to an alexa utterance.
+var xhr = function xhrWrapper() {
+    var request = new XMLHttpRequest({ mozSystem: true }); // Set Mozsystem to true in order not get error.
+    return function(method, url, callback) {
+        request.onreadystatechange = function() {
+            if (request.readyState === 4) {
+                callback(request.responseText);
+            }
+        };
+        request.open(method, url, true);
+        request.send();
+    };
+}();
 
-// Code to check api against server
-var ourRequest = new XMLHttpRequest({ mozSystem: true }); // Set Mozsystem to true in order not get error.
-ourRequest.open('GET', url);
-ourRequest.onload = function() {
-	var rhymes = JSON.parse(ourRequest.responseText); // Receive JSON data
-	var randomIndex = Math.floor(Math.random() * rhymes.length);
-	console.log(rhymes[randomIndex].word); // Pick random rhyme from JSON
-	result = rhymes[randomIndex].word;
-	alert(result); // Popup will change to an alexa return statement.
-};
-ourRequest.send();
+function getNRhymingWords(word, n) {
+	xhr('GET', 'https://api.datamuse.com/words?rel_rhy=' + word, function(responseText) {
+		debugger;
+		var rhymes = JSON.parse(responseText); // Receive JSON data
+		var rhymingWords = [];
+		for (var i = 0; i < n; i++) {
+			var randomIndex = Math.floor(Math.random() * rhymes.length);
+			rhymingWords.push(rhymes[randomIndex].word);
+		}
+		return rhymingWords;
+	});
+}
+
+var rhyme = prompt('Enter the word to rhyme with'); // FUTURE ALEXA UTTERANCE
+console.log('rhyming words API call result:', getNRhymingWords(rhyme, 5));
