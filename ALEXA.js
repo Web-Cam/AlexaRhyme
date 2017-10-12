0ce0d759d8b8afa8491d635e57b5c5a1083a7903
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2017-10-12 12:49:58 -0400

    Update ALEXA.js
    Change the messy code into a call to the rhymeGen function

--- ALEXA.js
+++ ALEXA.js
@@ -1,41 +1,13 @@
 var http = require('http')
 var Alexa = require('alexa-sdk');
 var start = 'https://api.datamuse.com/words?rel_rhy=';
+var rhymeGen = require(./main.js)
 const skillName = "Rhyme Generator";
 
 var handlers = {
 
     "GetRhyme": function () {
-		var rhyme = this.event.request.intent.slots.words;
-		var url = start + rhyme;
-       var xhr = function xhrWrapper() {
-    var request = new XMLHttpRequest({ mozSystem: true }); // Set Mozsystem to true in order not get error.
-    return function(method, url, callback) {
-        request.onreadystatechange = function() {
-            if (request.readyState === 4) {
-                callback(request.responseText);
-            }
-        };
-        request.open(method, url, true);
-        request.send();
-    };
-}();
-
-function getNRhymingWords(word, n) {
-	xhr('GET', 'https://api.datamuse.com/words?rel_rhy=' + word, function(responseText) {
-		debugger;
-		var rhymes = JSON.parse(responseText); // Receive JSON data
-		var rhymingWords = [];
-		for (var i = 0; i < n; i++) {
-			var randomIndex = Math.floor(Math.random() * rhymes.length);
-			rhymingWords.push(rhymes[randomIndex].word);
-		}
-		return rhymingWords;
-	});
-}
-this.emit(:tell,`your word' ${word}`,'rhymes with',rhymingWords);
-//var rhyme = this.event.request.intent.slots.words.value; // FUTURE ALEXA UTTERANCE
-//console.log('rhyming words API call result:', getNRhymingWords(rhyme, 5));
+    rhymeGen()
     },
 
     "AboutIntent": function () {
